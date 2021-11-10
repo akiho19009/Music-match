2d874964e6d8c999b562b42c3ca37ac30d30a8d0
akiho19009 <akiho811@gmail.com>
2021-11-10 10:31:15 +0000

    画像投稿途中

--- app/controllers/music_images_controller.rb
+++ app/controllers/music_images_controller.rb
@@ -1,18 +1,37 @@
 class MusicImagesController < ApplicationController
   
   def new
+    #インスタンス変数に空のインスタンスを渡して画像の投稿を可能にする
+    @music_image = MusicImage.new
   end
   
+  #投稿データの保存
   def create
+    #post_image_paramsでフォームで入力されたデータが投稿データとして許可されているパラメータかどうかをチェック
+    @music_image = MusicImage.new(music_image_params)
+    #ログイン中のユーザー情報の取得（deviceのヘルパーメソッド）
+    @music_image.user_id = current_user.id
+    @music_image.save
+    redirect_to music_image_path
   end
   
+  #投稿データの一覧表示
   def index
+    @music_images = MusicImage.all
   end
   
   def show
   end
   
   def destroy
+  end
+  
+  
+  #投稿データのストロングパラメータ
+  private
+  
+  def music_image_params
+    params.require(:music_image).permit(:name, :image, :caption)
   end
   
 end
